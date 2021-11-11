57d704e70c011547d9a8230461148a61837bc413
torsteinnesby <ttnesby@gmail.com>
2021-11-11 19:04:02 +0100

    ongoing

--- src/parseLandingzones.fsx
+++ src/parseLandingzones.fsx
@@ -6,10 +6,18 @@ open ALogger
 
 let resultAsExitCode = function | Error e -> ALog.err $"{e}"; 1 | Ok _ -> 0
 
+// verify that tenant mnggrpid integrity
+
 $"{__SOURCE_DIRECTORY__}/../landingzones.yaml" 
 |> LandingZoneYaml.tryList
 |> Result.bind LandingZone.tryList
 |> Result.bind LandingZone.uniqueNameAndType
+// verify that new tech contact is in Azure AD
+// verify that type of lz has not changed
+// load current set of json-files for online and hybrid
+// move decommissioned json-files to decommissioned
+// update existing json-files
+// generate new json-files
 |> resultAsExitCode
 |> fun ec ->
     ALog.inf $"Exit code: {ec}"
